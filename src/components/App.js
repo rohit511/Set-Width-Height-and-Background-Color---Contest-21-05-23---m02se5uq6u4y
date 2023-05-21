73f30b471c550060e910ba029af91593e239a128
Rohit511 <rathorerohit511@gmail.com>
2023-05-21 19:44:29 +0530

    mes123

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,23 @@ const App = () => {
   const [width, setWidth] = useState("");
   const [height, setHeight] = useState("");
   const [backgroundColor, setBackgroundColor] = useState("");
+  let [t,setT]=useState(false);
 
+  const ti=(event)=>{
+   setHeight(event.target.value);
+  }
+  const wi=(event)=>{
+    setWidth(event.target.value);
+  }
+  const co=(event)=>{
+    setBackgroundColor(event.target.value);
+  }
+const hi=()=>{
+    
+    console.log(height+""+width+""+backgroundColor);
+
+   setT(true);
+}
   return (
     <div id="main">
       <h1>Box Creator</h1>
@@ -14,6 +30,7 @@ const App = () => {
         <input
           id="width"
           type="number"
+          onChange={wi}
         />
       </div>
       <div>
@@ -21,17 +38,22 @@ const App = () => {
         <input
           id="height"
           type="number"
+          onChange={ti}
         />
+        
       </div>
       <div>
         <label htmlFor="backgroundColor">Background Color:</label>
         <input
           id="backgroundColor"
           type="text"
+          onChange={co}
+
         />
       </div>
-      <button className='create-btn'>Create Box</button>
-      <div id="box-container"></div>
+      <button className='create-btn' onClick={hi}>Create Box </button>
+      {t?<div id="box-conatiner" style={{height:`${height}px`,width:`${width}px`,backgroundColor:`${backgroundColor}`}}></div>
+:null}
     </div>
   );
 };
